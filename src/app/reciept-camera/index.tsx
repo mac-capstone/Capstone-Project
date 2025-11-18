08d896e0796f595d153a1d963609e048f64fcf2c
jay-esh <anilj@mcmaster.ca>
2025-11-17 22:13:56 -0500

    feat: camera-screen with gemini integration

---
+++ src/app/reciept-camera/index.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+import CameraViewComponent from './camera-view-test';
+
+export default function ReceiptCameraScreen() {
+  return (
+    <View className="flex-1">
+      <CameraViewComponent />
+    </View>
+  );
+}
